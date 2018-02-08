4bb82c74fbf1d6600e5bf27122a519e1620a3acb
Richard Spencer <Rlspenc15@earlham.edu>
2018-02-08 09:24:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String bookTitle;
+    private String author;
+    private String year;
+    private String isbn;
+    /*private image image (?)*/
+    private boolean in = true;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isIn() {
+        return in;
+    }
+
+    public void setIn(boolean in) {
+        this.in = in;
+    }
+}
---
+++ src/main/java/com/example/demo/LibraryRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+import org.springframework.data.repository.CrudRepository;
+public  interface LibraryRepository extends CrudRepository<MainController, Long>{
+
+}
+
